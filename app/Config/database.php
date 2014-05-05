a1434d8302963be98224b60e462fa9336f75cb55
tahamada <ahamada.tamou@gmail.com>
2014-05-05 13:46:26 +0200

    database clear

--- app/Config/database.php
+++ app/Config/database.php
@@ -3,50 +3,40 @@ class DATABASE_CONFIG {
 	public $default = array(
 	 "datasource" => "Database/Sqlserver",
 	 "persistent" => false,
-	 // "host" => "WIN7PRO-PC\SQLSERVER2008",
-	 "host" => "SERVEUR2008\SQLSERVER2008",
-	 "login" => "eReleveApplication",
-	 "password" => "123456",
-	 // "database" => "ECWP-eReleveData",
-	 "database" => "ECWP_ecoReleveData",
+	 "host" => "",
+	 "login" => "",
+	 "password" => "",
+	 "database" => "",
 	 "prefix" => ""
 	);
-	
-	public $mycoflore = array(
-		"datasource" => "Database/Sqlserver",
-		"persistent" => false,
-		"host" => "WIN7PRO-PC\WIN7SERVER",
 		
-		"database" => "DB_mycoflore"
-	);
-	
 	public $ereleveArgos = array(
 	 "datasource" => "Database/Sqlserver",
 	 "persistent" => false,
-	 "host" => "SERVEUR2008\SQLSERVER2008",
-	 "login" => "eReleveApplication",
-	 "password" => "123456",
-	 "database" => "ECWP_eReleve_Argos",
+	 "host" => "",
+	 "login" => "",
+	 "password" => "",
+	 "database" => "",
 	 "prefix" => ""
 	);	
 	
 	public $ereleveSensor= array(
 	 "datasource" => "Database/Sqlserver",
 	 "persistent" => false,
-	 "host" => "SERVEUR2008\SQLSERVER2008",
-	 "login" => "eReleveApplication",
-	 "password" => "123456",
-	 "database" => "ECWP_eReleve_Sensor",
+	 "host" => "",
+	 "login" => "",
+	 "password" => "",
+	 "database" => "",
 	 "prefix" => ""
 	);	
 	
 	public $taxon = array(
 	 "datasource" => "Database/Sqlserver",
 	 "persistent" => false,
-	 "host" => "SERVEUR2008\SQLSERVER2008",
-	 "login" => "eReleveApplication",
-	 "password" => "123456",
-	 "database" => "Taxon",
+	 "host" => "",
+	 "login" => "",
+	 "password" => "",
+	 "database" => "",
 	 "prefix" => "",
 	 "encoding" => "",
 	);
@@ -54,10 +44,10 @@ class DATABASE_CONFIG {
 	public $user = array(
 	 "datasource" => "Database/Sqlserver",
 	 "persistent" => false,
-	 "host" => "WIN7PRO-PC\SQLSERVER2008",
+	 "host" => "",
 	 "login" => "",
 	 "password" => "",
-	 "database" => "ECWP_TRACK_SECURITE",
+	 "database" => "",
 	 "prefix" => "",
 	 "encoding" => "",
 	);
